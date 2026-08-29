1c6f6d7b14bab83f2be2b171df3361cd863d1ba3
machiav3lli <antonios.hazim@tutanota.com>
2026-08-29 03:16:50 +0200

    Update: App expandable description UI of AppPage

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/appsheet/TextBlock.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/appsheet/TextBlock.kt
@@ -1,25 +1,16 @@
 package com.machiav3lli.fdroid.ui.components.appsheet
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.unit.dp
 import be.digitalia.compose.htmlconverter.htmlToAnnotatedString
-import com.machiav3lli.fdroid.R
-import com.machiav3lli.fdroid.ui.compose.utils.addIfElse
+import com.machiav3lli.fdroid.ui.components.ExpandableItemsBlock
 
 @Composable
 fun HtmlTextBlock(
@@ -28,34 +19,25 @@ fun HtmlTextBlock(
     longText: String = "",
     onUriClick: (String) -> Unit = {},
 ) {
-    Column(
+    val text = remember(shortText, longText) {
+        htmlToAnnotatedString(
+            longText,
+            linkInteractionListener = { link ->
+                if (link is LinkAnnotation.Url) onUriClick(link.url)
+            }
+        )
+    }
+
+    ExpandableItemsBlock(
         modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        heading = shortText
     ) {
-        var isExpanded by rememberSaveable { mutableStateOf(false) }
-        val text = remember(isExpanded, shortText, longText) {
-            htmlToAnnotatedString(
-                if (isExpanded) longText else shortText,
-                linkInteractionListener = { link ->
-                    if (link is LinkAnnotation.Url) onUriClick(link.url)
-                }
-            )
-        }
         Text(
             text,
             modifier = Modifier
                 .animateContentSize()
                 .padding(12.dp)
-                .addIfElse(
-                    isExpanded,
-                    { fillMaxWidth() },
-                    { align(Alignment.CenterHorizontally) }
-                )
+                .fillMaxWidth()
         )
-        if (longText.isNotEmpty()) {
-            FilledTonalButton(onClick = { isExpanded = !isExpanded }) {
-                Text(text = stringResource(id = if (isExpanded) R.string.show_less else R.string.show_more))
-            }
-        }
     }
 }
